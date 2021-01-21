6665f2295d624da9a49ad79c178d6109cdeb37fd
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-01-21 00:44:59 -0500

    fix csp?

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.HEROKU === 'true') {
 }
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
